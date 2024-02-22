190068d77409e5e87ec2d7a736bb74479cac2057
andreyscott <andreyscott301@gmail.com>
2024-02-22 20:56:38 +0100

    added the first screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meal_app/categories.dart';
+import 'package:meal_app/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -22,7 +22,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: theme,
       home: const CategoriesScreen(),
     );
---
+++ lib/screens/categories.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick Your Meal!'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 15,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+            )
+        ],
+      ),
+    );
+  }
+}

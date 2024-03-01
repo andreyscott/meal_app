dd8b7727374cecdedaa1df90ec0565401084e0e5
andreyscott <andreyscott301@gmail.com>
2024-03-01 09:53:24 +0100

    happy ne month

---
+++ lib/widgets/meal_item_trait.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class MealItemTrait extends StatelessWidget{
+
+  const MealItemTrait({super.key, required this.icon, required this.label,
+  });
+
+
+  final IconData icon;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+  return Row(
+    children: [
+      
+
+    ],
+  )
+  }
+
+}
